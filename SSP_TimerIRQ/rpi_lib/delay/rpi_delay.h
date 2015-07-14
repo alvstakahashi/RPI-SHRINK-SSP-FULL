1685ab107bf4857b8c13eaaa6abf6778a4eafe41
alvstakahashi <firstandlastandallways@gmail.com>
2015-07-15 08:53:48 +0900

    Revert "20150618"
    This reverts commit 7276b51202d6b5f1b535e3098890296fe268cbff.

--- SSP_TimerIRQ/rpi_lib/delay/rpi_delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef RPILIB_DELAY_H
-#define RPILIB_DELAY_H
-
-// delay
-void delayMicroseconds(unsigned int us);
-void delay(unsigned int ms);
-
-#endif
--- SSP_TimerIRQ/ssp/sample1.h
+++
@@ -1,7 +0,0 @@
-/*
- *  関数のプロトタイプ宣言
- */
-#ifndef TOPPERS_MACRO_ONLY
-extern void	task1(intptr_t arg);
-extern void	task2(intptr_t arg);
-#endif /* TOPPERS_MACRO_ONLY */
